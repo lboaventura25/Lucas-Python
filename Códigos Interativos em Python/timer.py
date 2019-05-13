556266e0e19bbd3cb5f51c1afecb420a94a5f6f5
Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
2019-05-13 18:30:54 -0300

    Add Python3.7 codes

--- Códigos Interativos em Python/timer.py
+++ Códigos Interativos em Python/timer.py
@@ -5,8 +5,8 @@ import random
     Código criado para facilitar a organização
     do evento EIGHT da Universidade de Brasília,
     onde cada palestrante tem até 8 minutos, 
-    e a partir do minuto digitado antes do cronometro 
-    começar começa a piscar a tela de 5 em 5 segundos.
+    e a partir do minuto digitado previamente,
+    o cronômetro começa a piscar a tela de 5 em 5 segundos.
 """
 
 SCREEN_WIDTH = 1850
